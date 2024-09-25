7a5d1380830821a0848b459aeab0ce1e933870ae
pedroadiaz <pedroadiaz@hotmail.com>
2024-09-25 14:31:44 -0700

    Renaming the schema

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const url = require('url');
 const fetch = require('node-fetch');
 
 const admin = "[user]";
-const tableName = "table_simplereports";
+const tableName = "gmrgfeoc_simplereports";
 const connectionPool = mysql.createPool({
     host: process.env.DATABASE_ENDPOINT,
     user: process.env.DATABASE_USER,
